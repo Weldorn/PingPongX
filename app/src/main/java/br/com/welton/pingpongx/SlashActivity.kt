44a71c641174e2ba868029559d1597d0ff9a4bd3
Welton Dornelas <dornelas.mag@gmail.com>
2020-09-01 21:30:47 -0300

    Adiciona navegação da splash screen

--- app/src/main/java/br/com/welton/pingpongx/SlashActivity.kt
+++ app/src/main/java/br/com/welton/pingpongx/SlashActivity.kt
@@ -1,11 +1,19 @@
 package br.com.welton.pingpongx
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.Handler
 
 class SlashActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_slash)
+
+        Handler().postDelayed({
+            val nextScreen = Intent(this, PlayerActivity::class.java)
+            startActivity(nextScreen)
+            finish()
+        },2000)
     }
 }
